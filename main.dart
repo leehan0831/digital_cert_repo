f722461f5c5c24e38635e5299ce75097b4730684
Kisharini <224618@student.upm.edu.my>
2025-07-01 00:03:13 +0800

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,52 @@
+import 'package:digital_cert/widgets/dashboard/viewer_dashboard.dart';
+import 'package:digital_cert/widgets/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Digital Certificate Viewer',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'SF Pro Display',
+        scaffoldBackgroundColor: const Color(0xFFF0F4F8),
+      ),
+      home: AuthGate(),
+    );
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // Check if user is authenticated
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        } else if (snapshot.hasData) {
+          return const ViewerDashboard();
+        } else {
+          return const LoginScreen(); 
+        }
+      },
+    );
+  }
+}
